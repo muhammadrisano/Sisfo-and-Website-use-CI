18145df2daef2001a318e861dba27ce970471a1b
Muhammad Risano Akbar <muhammadrisano@gmail.com>
2019-07-14 20:50:32 +0700

    menambah kedisiplinan

---
+++ application/controllers/Kedisiplinan.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+class Kedisiplinan extends CI_Controller
+{
+    public function index()
+    {
+        echo "ini halaman kedisiplinan";
+    }
+    public function kehadiran()
+    {
+        $data['title'] = "Website SMAN 4 Padang";
+        $this->load->view('front-page/header.php', $data);
+        $this->load->view('kedisiplinan/kehadiran.php', $data);
+        $this->load->view('front-page/footer.php', $data);
+    }
+    public function atribut()
+    {
+        $data['title'] = "Website SMAN 4 Padang";
+        $this->load->view('front-page/header.php', $data);
+        $this->load->view('kedisiplinan/atribut.php', $data);
+        $this->load->view('front-page/footer.php', $data);
+    }
+}
